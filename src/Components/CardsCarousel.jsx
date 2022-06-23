91d48eced98751e6ffe08d17cec71610a1f35db8
Barel Shraga <barel123133@gmail.com>
2022-06-23 20:47:28 +0300

    Bordered the carousel

--- src/Components/CardsCarousel.jsx
+++ src/Components/CardsCarousel.jsx
@@ -1,111 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-import { useMediaQuery } from 'react-responsive';
 import Card from './Card';
 import './CardsCarousel.scss';
 
-// Decalare variants for cards animation
-const cardVariant = (offsetX) => ({
-    previous: {
-        x: [offsetX, 0],
-        scale: [offsetX > 0 ? 0.4 : 1, 0.8],
-        opacity: [offsetX > 0 ? 0.1 : 1, 0.6],
-        transition: { duration: 0.4 },
-    },
-    prior: {
-        x: [offsetX, 0],
-        scale: [offsetX < 0 ? 0.4 : 1, 0.8],
-        opacity: [offsetX < 0 ? 0.1 : 1, 0.6],
-        transition: { duration: 0.4 },
-    },
-    active: {
-        x: [offsetX, 0],
-        scale: [0.8, 1],
-        opacity: [0.6, 1],
-        transition: { duration: 0.4 },
-    },
-    stopActive: {
-        x: 0,
-        scale: 1,
-        opacity: 1,
-    },
-    stop: { x: 0, scale: 0.8, opacity: 0.6 },
-});
-
 export default function CardsCarousel({ cards }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [offsetX, setoffsetX] = useState(0);
-    const [cardVariantName, setCardVariantName] = useState('stopActive');
+    const [width, setWidth] = useState(0);
 
-    // Get the previous and the prior cards
-    const currentIndexPrevius = currentIndex === 0 ? cards.length - 1 : currentIndex - 1;
-    const currentIndexPrior = currentIndex === cards.length - 1 ? 0 : currentIndex + 1;
+    const carousel = useRef();
 
-    // Slide cards every 10 secs
     useEffect(() => {
-        const sliderInterval = setInterval(() => {
-            setCurrentIndex(offsetX > 0 ? currentIndexPrevius : currentIndexPrior);
-            startCardAnimation();
-        }, 10000);
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
 
-        // Clear The interval on unmount
-        return () => clearInterval(sliderInterval);
-    }, [currentIndex]);
-
-    // Start animation on cards
-    const startCardAnimation = () => {
-        setCardVariantName('start');
-        const animationTimeOut = setTimeout(() => {
-            clearTimeout(animationTimeOut);
-            setCardVariantName('stop');
-        }, 100);
+    const moveCarousel = (back = false) => {
+        
     };
-
-    const moveCarousel = (cardIndex, x) => {
-        setCurrentIndex(cardIndex);
-        setoffsetX(x === offsetX ? x + 1 : x);
-        startCardAnimation();
-    };
-
-    const bigScreen = useMediaQuery({ query: `(max-width: 1200px)` });
 
     return (
         <div className='CardsCarousel'>
-            <motion.div className='cards' drag='x'>
-                {cards.map((card, i) => (
-                    <motion.div
-                        key={i}
-                        className='active-card'
-                        variants={cardVariant(offsetX)}
-                        animate={cardVariantName}>
-                        <Card card={card} />
-                    </motion.div>
-                ))}
-            </motion.div>
-            {/* <motion.div
-                    className='prior-card'
-                    variants={cardVariant(offsetX)}
-                    animate={cardVariantName === 'start' ? 'prior' : 'stop'}>
-                    <Card card={cards[currentIndexPrior]} />
-                </motion.div>
-                <div className='active-card'>
-                    <motion.div variants={cardVariant(offsetX)} animate={cardVariantName === 'start' ? 'active' : 'stopActive'}>
-                        <Card card={cards[currentIndex]} />
-                    </motion.div>
-                </div>
+            <motion.div className='carousel'>
                 <motion.div
-                    className='previous-card'
-                    variants={cardVariant(offsetX)}
-                    animate={cardVariantName === 'start' ? 'previous' : 'stop'}>
-                    <Card card={cards[currentIndexPrevius]} />
+                    className='carousel-inner'
+                    drag='x'
+                    dragConstraints={{ right: 0, left: -width }}
+                    ref={carousel}
+                    whileTap={{ cursor: 'grabbing' }}>
+                    {cards.map((card, i) => (
+                        <motion.div key={i} className='carousel-inner-item'>
+                            <Card card={card} />
+                        </motion.div>
+                    ))}
                 </motion.div>
-            </div> */}
+            </motion.div>
             <div className='cards-btns'>
-                <div onClick={() => moveCarousel(currentIndexPrior, bigScreen ? -300 : -400)}>
+                <div onClick={() => moveCarousel()}>
                     <HiChevronLeft />
                 </div>
-                <div onClick={() => moveCarousel(currentIndexPrevius, bigScreen ? 300 : 400)}>
+                <div onClick={() => moveCarousel(true)}>
                     <HiChevronRight />
                 </div>
             </div>
